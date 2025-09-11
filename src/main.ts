341de0037a0051f46efdeb4bf8af2f5ecac8196f
amir-razmi <amir.dv8212@gmail.com>
2025-09-11 16:59:25 +0330

    feat: activate dto validation for application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { env } from './env';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(env.PORT);
 }
 void bootstrap();
