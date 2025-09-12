d0221f3b349227560632862d17ae89ecef1e068c
amir-razmi <amir.dv8212@gmail.com>
2025-09-12 18:37:21 +0330

    feat: import all modules to appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 import { AdminGuard } from './common/guards/admin.guard';
+import { AuthModule } from './modules/auth/auth.module';
+import { RedirectModule } from './modules/redirect/redirect.module';
+import { UrlModule } from './modules/url/url.module';
+import { UserModule } from './modules/user/user.module';
+import { VisitModule } from './modules/visit/visit.module';
 
 @Module({
   providers: [
@@ -18,6 +23,6 @@ import { AdminGuard } from './common/guards/admin.guard';
       useClass: ClassSerializerInterceptor,
     },
   ],
-  imports: [],
+  imports: [AuthModule, RedirectModule, UrlModule, UserModule, VisitModule],
 })
 export class AppModule {}
