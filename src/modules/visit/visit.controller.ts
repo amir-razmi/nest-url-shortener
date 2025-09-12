43c486b42b20f9ab512806334d609d838bd857b7
amir-razmi <amir.dv8212@gmail.com>
2025-09-12 18:34:42 +0330

    feat: create visit controllers

--- src/modules/visit/visit.controller.ts
+++ src/modules/visit/visit.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, Req } from '@nestjs/common';
+import { VisitService } from './visit.service';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { type Request } from 'express';
+import { AdminRoute } from 'src/common/decorators/admin-route.decorator';
 
 @Controller('visit')
-export class VisitController {}
+export class VisitController {
+  constructor(private visitService: VisitService) {}
+
+  @Get(':urlId')
+  async getAllVisitsByUrlId(
+    @Query() query: PaginationDto,
+    @Param('urlId') urlId: string,
+    @Req() req: Request,
+  ) {
+    return this.visitService.getVisitsByUrlId(query, urlId, req.user.userId);
+  }
+
+  @AdminRoute()
+  @Get('all')
+  async getAllVisits(@Query() query: PaginationDto) {
+    return this.visitService.getAllVisits(query);
+  }
+}
