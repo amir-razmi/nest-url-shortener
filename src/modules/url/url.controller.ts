513e20746a66ac7bfd18915c40495e7967df3100
amir-razmi <amir.dv8212@gmail.com>
2025-09-12 18:02:59 +0330

    feat: create all  url controllers

--- src/modules/url/url.controller.ts
+++ src/modules/url/url.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { UrlService } from './url.service';
+import { CreateShortUrlDto } from './dto/create-short-url.dto';
+import { type Request } from 'express';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { AdminRoute } from 'src/common/decorators/admin-route.decorator';
 
 @Controller('url')
-export class UrlController {}
+export class UrlController {
+  constructor(private urlService: UrlService) {}
+
+  @Post('create')
+  createShortUrl(@Body() body: CreateShortUrlDto, @Req() req: Request) {
+    return this.urlService.createShortUrl(body, req.user.userId);
+  }
+
+  @Delete('delete/:id')
+  deleteShortUrl(@Req() req: Request, @Param('id') id: string) {
+    return this.urlService.deleteShortUrl(id, req.user.userId);
+  }
+
+  @Get('all')
+  getAllUrls(@Req() req: Request, @Query() query: PaginationDto) {
+    return this.urlService.getAllUrls(query, req.user.userId);
+  }
+
+  @AdminRoute()
+  @Get('admin/all')
+  getAllUrlsAdmin(@Query() query: PaginationDto) {
+    return this.urlService.getAllUrls(query);
+  }
+}
