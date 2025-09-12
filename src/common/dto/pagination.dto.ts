23ed792ff12ea6fe98c69011a6e78fbe73e7f0ee
amir-razmi <amir.dv8212@gmail.com>
2025-09-12 21:06:05 +0330

    fix: fix pagination swagger doc

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -2,13 +2,13 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsOptional, IsPositive, Max, Min } from 'class-validator';
 
 export class PaginationDto {
-  @ApiProperty({ default: 1, minimum: 0 })
+  @ApiProperty({ default: 1, minimum: 0, type: 'integer' })
   @IsInt()
   @Min(0)
   @IsOptional()
   page = 1;
 
-  @ApiProperty({ default: 10, minimum: 1, maximum: 100 })
+  @ApiProperty({ default: 10, minimum: 1, maximum: 100, type: 'integer' })
   @IsInt()
   @IsPositive()
   @Max(100)
