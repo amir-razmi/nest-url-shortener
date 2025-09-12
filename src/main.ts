cbd5bfff03b623ececd74bc9be8b866324e48a84
amir-razmi <amir.dv8212@gmail.com>
2025-09-12 17:03:01 +0330

    feat: create auth guard and apply it globally

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { env } from './env';
-import { ValidationPipe } from '@nestjs/common';
-import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
