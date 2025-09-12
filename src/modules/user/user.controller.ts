f3d3fc08d6f4eb99661942512828d10c86652882
amir-razmi <amir.dv8212@gmail.com>
2025-09-12 17:33:32 +0330

    feat: create getAllUsers controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Req } from '@nestjs/common';
+import { Controller, Get, Query, Req } from '@nestjs/common';
 import { type Request } from 'express';
 import { UserService } from './user.service';
 import { GetProfileResponseDto } from './dto/get-profile-response.dto';
 import { plainToInstance } from 'class-transformer';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
+import { AdminRoute } from 'src/common/decorators/admin-route.decorator';
 
 @Controller('user')
 export class UserController {
@@ -12,5 +14,12 @@ export class UserController {
   async getProfile(@Req() req: Request) {
     const user = await this.userService.getUserById(req.user.userId);
     return plainToInstance(GetProfileResponseDto, user);
+  }
+
+  @AdminRoute()
+  @Get('all')
+  async getAllUsers(@Query() query: PaginationDto) {
+    const users = await this.userService.getAllUsers(query);
+    return plainToInstance(GetProfileResponseDto, users);
   }
 }
