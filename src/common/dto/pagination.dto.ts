83a971f77b6f43df1ce6b1f9dbcc0f10e11f9c77
amir-razmi <amir.dv8212@gmail.com>
2025-09-14 12:34:55 +0330

    fix: fix pagination's dto bug

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,14 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 import { IsInt, IsOptional, IsPositive, Max, Min } from 'class-validator';
 
 export class PaginationDto {
   @ApiProperty({ default: 1, minimum: 0, type: 'integer' })
+  @Transform(({ value }) => +value)
   @IsInt()
   @Min(0)
   @IsOptional()
   page = 1;
 
   @ApiProperty({ default: 10, minimum: 1, maximum: 100, type: 'integer' })
+  @Transform(({ value }) => +value)
   @IsInt()
   @IsPositive()
   @Max(100)
