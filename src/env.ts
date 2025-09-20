9d8a8fbcb9edcca849687dbf5ca9cfe785d17e52
amir-razmi <amir.dv8212@gmail.com>
2025-09-20 13:50:20 +0330

    fix: fix env schema's error in test environment

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,10 @@
-import { createEnv } from '@t3-oss/env-core';
 import { config } from 'dotenv';
 import { z } from 'zod';
 
 config();
 
-export const env = createEnv({
-  server: {
+export const env = z
+  .object({
     NODE_ENV: z.enum(['development', 'test', 'production']),
     CORS: z.string().min(1),
     PORT: z.coerce.number().default(5000),
@@ -16,7 +15,5 @@ export const env = createEnv({
     REDIS_HOST: z.string().min(1),
     REDIS_PORT: z.coerce.number().default(6379),
     REDIS_PASSWORD: z.string().optional(),
-  },
-  runtimeEnv: process.env,
-  emptyStringAsUndefined: true,
-});
+  })
+  .parse(process.env);
