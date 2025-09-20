3c91cb7c9fb90a18de2ac1e50046e0a61ee58ae2
amir-razmi <amir.dv8212@gmail.com>
2025-09-20 13:49:59 +0330

    feat: separate jest's config

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+
+  // Added alias support for src/*
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+};
