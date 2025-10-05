2f65e7c377de989a4816e58a6f5317815bd02e59
amir-razmi <amir.dv8212@gmail.com>
2025-10-05 14:00:12 +0330

    feat: complete authentication integration tests

--- test/integration/auth.int-spec.ts
+++ test/integration/auth.int-spec.ts
@@ -12,6 +12,20 @@ const testUser = {
   password: 'P@ssw0rd',
 };
 
+export const registerUser = async (app: INestApplication, verifyEmail = false) => {
+  await request(app.getHttpServer()).post('/auth/register').send(testUser).expect(201);
+
+  if (verifyEmail) {
+    const redis = app.get(RedisService);
+
+    const token = await redis.verificationTokenService.get(testUser.email.toLowerCase());
+    await request(app.getHttpServer())
+      .post('/auth/verify-email')
+      .send({ email: testUser.email, token })
+      .expect(201);
+  }
+};
+
 describe('Auth (int)', () => {
   let prisma: PrismaService;
   let redis: RedisService;
@@ -47,6 +61,8 @@ describe('Auth (int)', () => {
   });
 
   it('/auth/register  should register user', async () => {
+    const startTime = Date.now();
+
     const res = await request(app.getHttpServer()).post('/auth/register').send(testUser).expect(201);
     expect(res.body).toHaveProperty('message');
 
@@ -66,5 +82,160 @@ describe('Auth (int)', () => {
       testUser.email.toLowerCase(),
     );
     expect(verificationLastSentAtInRedis).toBeTruthy();
+    expect(+verificationLastSentAtInRedis!).toBeGreaterThanOrEqual(startTime);
+  });
+  it('/auth/register  should not allow duplicate emails', async () => {
+    await registerUser(app);
+
+    await request(app.getHttpServer())
+      .post('/auth/register')
+      .send({ ...testUser, username: 'another' + testUser.username })
+      .expect(409);
+  });
+  it('/auth/register  should not allow duplicate usernames', async () => {
+    await registerUser(app);
+
+    await request(app.getHttpServer())
+      .post('/auth/register')
+      .send({ ...testUser, email: 'another' + testUser.email })
+      .expect(409);
+  });
+  it('/auth/register  should allow re-registration if email not verified in 24 hours', async () => {
+    await registerUser(app);
+
+    //! I'm not using mocking of time here because it caused some issues with prisma client
+
+    const userInDb = await prisma.user.findUnique({ where: { email: testUser.email.toLowerCase() } });
+    await prisma.user.update({
+      where: { id: userInDb?.id },
+      data: { createdAt: new Date(Date.now() - 1000 * 60 * 60 * 25) }, //Set createdAt to 25 hours ago
+    });
+
+    await request(app.getHttpServer())
+      .post('/auth/register')
+      .send({ ...testUser, username: 'another' + testUser.username })
+      .expect(201);
+  });
+
+  it('/auth/verify-email  should verify email', async () => {
+    await registerUser(app);
+
+    const token = await redis.verificationTokenService.get(testUser.email.toLowerCase());
+    expect(token).toBeTruthy();
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/verify-email')
+      .send({ email: testUser.email, token })
+      .expect(201);
+    expect(res.body).toHaveProperty('message');
+
+    const userInDb = await prisma.user.findUnique({
+      where: { email: testUser.email.toLowerCase() },
+    });
+    expect(userInDb).toBeTruthy();
+    expect(userInDb?.isEmailVerified).toBe(true);
+
+    const verificationTokenInRedis = await redis.verificationTokenService.get(testUser.email.toLowerCase());
+    expect(verificationTokenInRedis).toBeNull();
+  });
+
+  it('/auth/verify-email  should not verify with invalid token', async () => {
+    await registerUser(app);
+
+    await request(app.getHttpServer())
+      .post('/auth/verify-email')
+      .send({ email: testUser.email, token: 'invalid' })
+      .expect(400);
+
+    const userInDb = await prisma.user.findUnique({
+      where: { email: testUser.email.toLowerCase() },
+    });
+    expect(userInDb).toBeTruthy();
+    expect(userInDb?.isEmailVerified).toBe(false);
+  });
+  it('/auth/resend-verification-email  should resend verification email', async () => {
+    const startTime = Date.now();
+
+    await registerUser(app);
+
+    const firstToken = await redis.verificationTokenService.get(testUser.email.toLowerCase());
+    expect(firstToken).toBeTruthy();
+
+    const firstSentAt = await redis.verificationTokenService.getLastSentAt(testUser.email.toLowerCase());
+    expect(firstSentAt).toBeTruthy();
+    expect(+firstSentAt!).toBeGreaterThanOrEqual(startTime);
+
+    await redis.verificationTokenService.delete(testUser.email.toLowerCase()); //Simulate token expiry by deleting it
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/resend-verification-email')
+      .send({ email: testUser.email })
+      .expect(201);
+    expect(res.body).toHaveProperty('message');
+
+    const secondToken = await redis.verificationTokenService.get(testUser.email.toLowerCase());
+    expect(secondToken).toBeTruthy();
+    expect(secondToken).not.toBe(firstToken);
+
+    const secondSentAt = await redis.verificationTokenService.getLastSentAt(testUser.email.toLowerCase());
+    expect(secondSentAt).toBeTruthy();
+    expect(+secondSentAt!).toBeGreaterThanOrEqual(startTime);
+  });
+  it('/auth/resend-verification-email  should not resend too frequently', async () => {
+    const startTime = Date.now();
+
+    await registerUser(app);
+
+    const firstToken = await redis.verificationTokenService.get(testUser.email.toLowerCase());
+    expect(firstToken).toBeTruthy();
+
+    const firstSentAt = await redis.verificationTokenService.getLastSentAt(testUser.email.toLowerCase());
+    expect(firstSentAt).toBeTruthy();
+    expect(+firstSentAt!).toBeGreaterThanOrEqual(startTime);
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/resend-verification-email')
+      .send({ email: testUser.email })
+      .expect(201);
+    expect(res.body).toHaveProperty('message');
+    expect(res.body).toHaveProperty('retryAfter');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    expect(res.body.retryAfter).toBeGreaterThan(+firstSentAt!);
+
+    const secondToken = await redis.verificationTokenService.get(testUser.email.toLowerCase());
+    expect(secondToken).toBeTruthy();
+    expect(secondToken).toBe(firstToken);
+
+    const secondSentAt = await redis.verificationTokenService.getLastSentAt(testUser.email.toLowerCase());
+    expect(secondSentAt).toBeTruthy();
+    expect(secondSentAt).toBe(firstSentAt);
+  });
+  it('/auth/login should login verified user', async () => {
+    await registerUser(app, true); //Register and verify
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ email: testUser.email, password: testUser.password })
+      .expect(201);
+    expect(res.body).toHaveProperty('message');
+    expect(res.headers).toHaveProperty('set-cookie');
+    expect(Array.isArray(res.headers['set-cookie'])).toBe(true);
+    expect(res.headers['set-cookie'][0]).toMatch(/accessToken=/);
+  });
+  it('/auth/login should not login unverified user', async () => {
+    await registerUser(app); //Register but do not verify
+
+    await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ email: testUser.email, password: testUser.password })
+      .expect(403);
+  });
+  it('/auth/login should not login with wrong password', async () => {
+    await registerUser(app, true); //Register and verify
+
+    await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ email: testUser.email, password: 'wrong' + testUser.password })
+      .expect(401);
   });
 });
