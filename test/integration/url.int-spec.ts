8975f76df6f22f4710078b1cd4ef326971f1cb8f
amir-razmi <amir.dv8212@gmail.com>
2025-12-03 16:50:33 +0330

    create url integration test file

---
+++ test/integration/url.int-spec.ts
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AppModule } from 'src/app.module';
+import { PrismaService } from 'src/common/db/prisma/prisma.service';
+import { RedisService } from 'src/common/db/redis/redis.service';
+import request from 'supertest';
+import { getAccessTokenCookie } from './auth.int-spec';
+import cookieParser from 'cookie-parser';
+
+describe('URL (int)', () => {
+  let prisma: PrismaService;
+  let redis: RedisService;
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    app.use(cookieParser());
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    await app.init();
+
+    prisma = moduleRef.get<PrismaService>(PrismaService);
+
+    redis = moduleRef.get<RedisService>(RedisService);
+    await redis.client.flushall();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  beforeEach(async () => {
+    await prisma.cleanDb();
+  });
+
+  it('should create short url', async () => {
+    const accessToken = await getAccessTokenCookie(app);
+
+    console.log('ACCESS TOKEN : ', accessToken);
+    const res = await request(app.getHttpServer())
+      .post('/url/create')
+      .set('Cookie', accessToken)
+      .send({ originalUrl: 'https://nestjs.com' })
+      .expect(201);
+
+    console.log(res.body);
+  });
+});
