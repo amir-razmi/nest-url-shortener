aa82f462def196e45b70ac081d05b71859943b23
amir-razmi <amir.dv8212@gmail.com>
2025-12-25 21:24:45 +0330

    update workflow

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'prisma/config';
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: 'mongodb://127.0.0.1:27018/url-shortner-test?directConnection=true',
+    // url: 'mongodb://127.0.0.1:27018/url-shortner-test?directConnection=true',
+    url: process.env.DATABASE_URL,
   },
 });
